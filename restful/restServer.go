b5c453c817fdc1471e99a977b886ef8edd631346
Andrew Poydence <apoydence@gmail.com>
2015-02-09 22:16:46 -0700

    Uses gorilla library, moves everything to single endpoint, adds remove, uses json

--- restful/restServer.go
+++ restful/restServer.go
@@ -1,54 +1,84 @@
 package restful
 
 import (
+	"encoding/json"
 	"github.com/apoydence/talaria"
+	"github.com/gorilla/pat"
+	"io"
 	"net/http"
-	"strconv"
+	"path"
+	"sync"
 )
+
+var once sync.Once
 
 type QueueHolder interface {
 	AddQueue(queueName string, size talaria.BufferSize) error
 	Fetch(queueName string) talaria.Queue
+	RemoveQueue(queueName string)
 }
 
 type RestServer struct {
 	queueHolder QueueHolder
 	addr        string
+	router      *pat.Router
+	jsonDecoder json.Decoder
+}
+
+type QueueData struct {
+	QueueName string             `json:"queueName"`
+	Buffer    talaria.BufferSize `json:"bufferSize"`
 }
 
 func StartNewRestServer(queueHolder QueueHolder, addr string) (*RestServer, <-chan error) {
 	server := &RestServer{
 		queueHolder: queueHolder,
 		addr:        addr,
+		router:      pat.New(),
 	}
 	return server, server.start()
 }
 
 func (rs *RestServer) start() <-chan error {
-	http.HandleFunc("/addQueue", rs.handleFetch)
-
 	errChan := make(chan error)
-	go func() {
-		errChan <- http.ListenAndServe(rs.addr, nil)
-	}()
+	once.Do(func() {
+		rs.router.Post("/queues", rs.handleAddQueue)
+		rs.router.Delete("/queues", rs.handleRemove)
+		http.Handle("/", rs.router)
+
+		go func() {
+			errChan <- http.ListenAndServe(rs.addr, nil)
+		}()
+	})
 	return errChan
 }
 
-func (rs *RestServer) handleFetch(resp http.ResponseWriter, req *http.Request) {
-	if req.Method == "POST" {
-		err := req.ParseForm()
-		if err != nil {
-			resp.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		if name, bufferSize, ok := addQueueParameters(req.PostForm); ok {
-			err := rs.queueHolder.AddQueue(name, bufferSize)
-			resp.WriteHeader(fetchStatusCode(err))
-			return
-		}
+func (rs *RestServer) handleAddQueue(resp http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		resp.WriteHeader(http.StatusBadRequest)
+		return
 	}
-	resp.WriteHeader(http.StatusNotFound)
+
+	dec := json.NewDecoder(req.Body)
+	var data QueueData
+	if err = dec.Decode(&data); err == io.EOF || err == nil {
+		err = rs.queueHolder.AddQueue(data.QueueName, data.Buffer)
+		resp.WriteHeader(fetchStatusCode(err))
+		return
+	}
+
+	resp.WriteHeader(http.StatusBadRequest)
+}
+
+func (rs *RestServer) handleRemove(resp http.ResponseWriter, req *http.Request) {
+	base, name := path.Split(req.URL.Path)
+	if base != "/queues/" || len(name) == 0 {
+		resp.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	rs.queueHolder.RemoveQueue(name)
+	resp.WriteHeader(http.StatusOK)
 }
 
 func fetchStatusCode(err error) int {
@@ -57,23 +87,4 @@ func fetchStatusCode(err error) int {
 	} else {
 		return http.StatusBadRequest
 	}
-}
-
-func addQueueParameters(form map[string][]string) (string, talaria.BufferSize, bool) {
-	var values []string
-	var ok bool
-	if values, ok = form["name"]; !ok || len(values) != 1 {
-		return "", -1, false
-	}
-	name := values[0]
-
-	if values, ok = form["bufferSize"]; !ok || len(values) != 1 {
-		return "", -1, false
-	}
-	bufferSize, err := strconv.Atoi(values[0])
-	if err != nil {
-		return "", -1, false
-	}
-
-	return name, talaria.BufferSize(bufferSize), true
 }
--- restful/restServer_test.go
+++ restful/restServer_test.go
@@ -1,37 +1,60 @@
 package restful_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/apoydence/talaria"
 	. "github.com/apoydence/talaria/restful"
 	"net/http"
-	"net/url"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
+var mqh = func() *mockQueueHolder {
+	mqh := &mockQueueHolder{}
+	StartNewRestServer(mqh, ":8080")
+	return mqh
+}()
+
 var _ = Describe("RestServer", func() {
 	Context("AddQueue", func() {
 		It("Should create a new queue", func() {
-			mqh := &mockQueueHolder{}
-			_, errChan := StartNewRestServer(mqh, ":8080")
-			resp, err := http.PostForm("http://localhost:8080/addQueue", url.Values{
-				"name":       {"some-queue"},
-				"bufferSize": {"5"},
+			j, _ := json.Marshal(QueueData{
+				QueueName: "some-queue",
+				Buffer:    5,
 			})
+			resp, err := http.Post("http://localhost:8080/queues", "application/json", bytes.NewReader(j))
 			Expect(err).To(BeNil())
-			Expect(resp.StatusCode).To(Equal(200))
-			Expect(errChan).ShouldNot(Receive())
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
 			Expect(mqh.addQueueName).To(Equal("some-queue"))
 			Expect(mqh.addBufferSize).To(BeEquivalentTo(5))
+		})
+	})
+	Context("Remove", func() {
+		It("Should call Remove on the QueueHolder", func() {
+			url := "http://localhost:8080/queues/someQueue"
+			req, _ := http.NewRequest("DELETE", url, nil)
+			resp, err := http.DefaultClient.Do(req)
+			Expect(err).To(BeNil())
+			Expect(resp.StatusCode).To(Equal(http.StatusOK))
+			Expect(mqh.removeQueueName).To(Equal("someQueue"))
+		})
+		It("Should return a BadRequest if the queue name is left off", func() {
+			url := "http://localhost:8080/queues"
+			req, _ := http.NewRequest("DELETE", url, nil)
+			resp, err := http.DefaultClient.Do(req)
+			Expect(err).To(BeNil())
+			Expect(resp.StatusCode).To(Equal(http.StatusBadRequest))
 		})
 	})
 })
 
 type mockQueueHolder struct {
-	addQueueName   string
-	fetchQueueName string
-	addBufferSize  talaria.BufferSize
+	addQueueName    string
+	removeQueueName string
+	fetchQueueName  string
+	addBufferSize   talaria.BufferSize
 }
 
 func (mqh *mockQueueHolder) AddQueue(queueName string, bufferSize talaria.BufferSize) error {
@@ -43,4 +66,8 @@ func (mqh *mockQueueHolder) AddQueue(queueName string, bufferSize talaria.Buffer
 func (mqh *mockQueueHolder) Fetch(queueName string) talaria.Queue {
 	mqh.fetchQueueName = queueName
 	return nil
+}
+
+func (mqh *mockQueueHolder) RemoveQueue(queueName string) {
+	mqh.removeQueueName = queueName
 }
