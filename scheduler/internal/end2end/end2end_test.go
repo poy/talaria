052abcb81dc8869567fce85514643e740d7aa19b
Andrew Poydence <apoydence@gmail.com>
2016-11-24 07:14:31 -0700

    Remove commented out test

--- scheduler/internal/end2end/end2end_test.go
+++ scheduler/internal/end2end/end2end_test.go
@@ -88,38 +88,6 @@ func TestSchedulerEnd2End(t *testing.T) {
 	})
 }
 
-// var _ = Describe("End2end", func() {
-// 	Describe("Create()", func() {
-// 		var (
-// 			createInfo *pb.CreateInfo
-// 		)
-
-// 		BeforeEach(func() {
-// 			createInfo = &pb.CreateInfo{
-// 				Name: createName(),
-// 			}
-// 		})
-
-// 		JustBeforeEach(func() {
-// 			testhelpers.AlwaysReturn(mockServer.CreateOutput.Ret0, new(intra.CreateResponse))
-// 			close(mockServer.CreateOutput.Ret1)
-// 		})
-
-// 		Context("Create() doesn't return an error", func() {
-// 			It("selects Node to create buffer via intra API", func() {
-// 				resp, err := schedulerClient.Create(context.Background(), createInfo)
-// 				Expect(err).ToNot(HaveOccurred())
-// 				expected := &intra.CreateInfo{
-// 					Name: createInfo.Name,
-// 				}
-
-// 				Expect(resp.Uri).To(Equal(fmt.Sprintf("localhost:%d", intraPort)))
-// 				Eventually(mockServer.CreateInput.Arg1).Should(BeCalled(With(expected)))
-// 			})
-// 		})
-// 	})
-// })
-
 func createName() string {
 	return fmt.Sprintf("some-buffer-%d", rand.Int63())
 }
